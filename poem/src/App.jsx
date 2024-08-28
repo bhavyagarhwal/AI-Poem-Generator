aa281c9eea00cfbb5f2768c4c96e264f4eb1d0a2
bhavyagarhwal <garhwalbhavya5@gmail.com>
2024-08-28 23:52:41 +0530

    frontend url changed2

--- poem/src/App.jsx
+++ poem/src/App.jsx
@@ -19,9 +19,7 @@ function App() {
 
   useEffect(() => {
     // Initialize WebSocket connection
-    const socket = new WebSocket(
-      "ws://ai-poem-generator-q7tj.onrender.com//ws"
-    );
+    const socket = new WebSocket("ws://ai-poem-generator-q7tj.onrender.com/ws");
 
     socket.onopen = () => {
       console.log("WebSocket connection established");
